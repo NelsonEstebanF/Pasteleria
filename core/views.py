3b443e641c94defa8ac49f69d4543577c9e3f0f4
NelsonEstebanF <esteban1922@hotmail.com>
2021-09-27 21:44:22 -0300

    terminado sin carga bd

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Postre
 
 def masita(request):
-    prostrecitos = Postre().objects.all()
+    prostrecitos = Postre.objects.all()
     return render(request, 'core/masita.html', {'postres': prostrecitos})
